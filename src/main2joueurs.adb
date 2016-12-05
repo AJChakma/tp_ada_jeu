af8b2fb7a9f5c017a240b691f6c2fdc15a413371
Welgriv <vincent.morice66@gmail.com>
2016-12-05 11:28:04 +0100

    instanciation de moteur de jeu dans main2joueurs, eval et coup possible à completer

--- src/main2joueurs.adb
+++ src/main2joueurs.adb
@@ -4,7 +4,7 @@ with Puissance4;
 with Participant;
 with Partie;
 with Liste_Generique;
--- with Moteur_Jeu;
+with Moteur_Jeu;
 
 use Ada.Text_IO;
 use Ada.Integer_Text_IO;
@@ -14,6 +14,45 @@ procedure Main2Joueurs is
    
    package MyPuissance4 is new Puissance4(4,4,4);
    
+   --INSTANCIATIONS POUR JOUER CONTRE L'IA
+   --------------------------------------------------------------------------------
+   
+   -- Instanciation d'une liste de coups qui seront les coups possibles
+   package Liste_Coups is new Liste_Generique(MyPuissance4.Coup,
+					      MyPuissance4.Affiche_Coup);
+   use Liste_Coups;
+   
+   --  Retourne une listes de coups possibles pour le joueur J
+   function Coups_Possibles(E : MyPuissance4.Etat; J : Joueur) return Liste is
+      L : Liste;
+   begin
+     return L;				--  à compléter
+   end Coups_Possibles;
+   
+   --  Fonction d'évaluation statique de l'état
+   function Eval(E: MyPuissance4.Etat) return Integer is
+      Res : Integer;
+   begin
+      return Res;			--  à compléter
+   end Eval;
+   
+   --  définition d'une IA joueur1 pour adverssaire avec le moteur de jeu
+      package IA_Player is new Moteur_Jeu(MyPuissance4.Etat,
+					  MyPuissance4.Coup,
+					  MyPuissance4.Jouer,
+					  MyPuissance4.Est_Gagnant,
+					  MyPuissance4.Est_Nul,
+					  MyPuissance4.Affiche_Coup,
+					  Liste_Coups,
+					  Coups_Possibles,
+					  Eval
+					  ,3 --  Profondeur
+					    ,Joueur1);
+      
+      use IA_Player;
+      
+      --------------------------------------------------------------------------------
+      
    -- definition d'une partie entre un humain en Joueur 1 et un humain en Joueur 2
    package MyPartie is new Partie(MyPuissance4.Etat,
 				  MyPuissance4.Coup, 
